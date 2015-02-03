e58a324e9b8d25b97bd58ee9049ff23655bb8112
kjacobson <kylejacobson1987@gmail.com>
2015-02-03 13:16:38 -0800

    allow controller to yield (e.g. to an extending class) the responsibility of sending the response to the client

--- examples/helloworld/lib/controllers/hello.js
+++ examples/helloworld/lib/controllers/hello.js
@@ -24,5 +24,10 @@ util.inherits(HelloController, Controller);
 HelloController.prototype.index = function (request, response, next) {
   this.render(request, response, 'hello/index');
 }
+HelloController.prototype.new   = function (request, response, next) {
+  this.render(request, response, 'hello/new', null, false).done(function(val) {
+    response.send(val);
+  });
+}
 
 module.exports = HelloController;
