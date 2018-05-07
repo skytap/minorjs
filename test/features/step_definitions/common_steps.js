292aeb2a8880c1de932dcb63b992dfdcf8e822f1
Michelle Nightward <mnightward@skytap.com>
2018-05-07 11:56:14 -0700

    update minorjs-test version and adding tests for manual and nozombie flags

--- test/features/step_definitions/common_steps.js
+++ test/features/step_definitions/common_steps.js
@@ -1,5 +1,6 @@
 var should         = require('should'),
     FunctionalTest = require('../../lib/functional_test');
+    Promise        = require('bluebird');
 
 var CommonSteps = module.exports = function () {
   this.Given(/^I am on the hello page$/, function () {
@@ -16,6 +17,10 @@ var CommonSteps = module.exports = function () {
 
   this.Given(/^I am on the frames page$/, function () {
     return FunctionalTest.browser.visit(FunctionalTest.getUrl('/frames'))
+  });
+
+  this.When(/^the step fails$/, function () {
+    return Promise.reject()
   });
 
   this.Then(/^the page URL is "([^"]*)"$/, function (text, next) {
